97551359133e5105ef5e14dfabe7f2e9306d8f5b
sneeu-leeu <sa070197@gmail.com>
2021-10-06 13:10:34 +0200

    Create Medical History table

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -1,5 +1,15 @@
 CREATE TABLE patients(
-id INT GENERATED ALWAYS AS IDENTITY,
-name VARCHAR(100) NOT NULL,
-date_of_birth date NOT NULL,
-PRIMARY KEY(id));
+  id INT GENERATED ALWAYS AS IDENTITY,
+  name VARCHAR(100) NOT NULL,
+  date_of_birth date NOT NULL,
+  PRIMARY KEY(id)
+);
+
+CREATE TABLE medical_histories (
+  id INT GENERATED ALWAYS AS IDENTITY,
+  atmitted_at TIMESTAMP,
+  patient_id INT,
+  status VARCHAR(100) NOT NULL
+  FOREIGN KEY (patient_id) REFERENCES patients (id),
+  PRIMARY KEY id,
+);
