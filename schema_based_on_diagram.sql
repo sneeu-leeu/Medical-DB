7d1f5a287cc20c50186c9cacadd4bdd6296b68ec
sheylaPozo <shiggipoke@gmail.com>
2021-10-06 08:08:23 -0500

    add create invoice items table, and connection table for treatments

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -24,10 +24,28 @@ CREATE TABLE invoices (
   PRIMARY KEY(id)
 );
 
+CREATE TABLE treatment(
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY
+  type VARCHAR(35), 
+  name VARCHAR(35)
+);
+
 CREATE TABLE treatments(
-id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY
-medical_history_id INT,
-treatment_id INT,
-FOREIGN KEY(medical_history_id) REFERENCES medical_histories(id),
-FOREIGN KEY(treatment_id) REFERENCES treatment(id)
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY
+  medical_history_id INT,
+  treatment_id INT,
+  FOREIGN KEY(medical_history_id) REFERENCES medical_histories(id),
+  FOREIGN KEY(treatment_id) REFERENCES treatment(id)
+);
+
+CREATE TABLE  invoice_items(
+  id INT GENERATED ALWAYS AS IDENTITY,
+  unit_prices FLOAT NOT NULL,
+  quantity INT NOT NULL,
+  total_price FLOAT NOT NULL,
+  invoice_id INT NOT NULL,
+  treatment_id INT NOT NULL,
+  FOREIGN KEY (invoice_id) REFERENCES invoice(id), 
+  FOREIGN KEY (treatment_id) REFERENCES treatment(id),
+  PRIMARY KEY (id)
 );
